1cad0217dc4d1e3537d1ab69e10530e0fd90a082
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2025-01-20 17:28:29 -0500

    Add next generation javascript and typescript

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,26 @@
-console.log('Your code goes here...');
+// class Person {
+//   name: string;
+//   age: number;
+
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+
+// const user = new Person('Andrey', 39);
+
+// console.log(user);
+
+// Default value
+// const add = (a: number, b: number = 1) => {
+//   return a + b;
+// };
+
+// console.log(add(5));
+
+// Rest
+// const add = (...numbers: number[]) =>
+//   numbers.reduce((acc, num) => acc + num, 0);
+
+// console.log(add(1, 2, 3, 4, 5));
